4d7b98c9900f5075ec8e45be0c4cdd3f5f76a424
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-02 21:45:25 -0300

    Using concat to concatenate two arrays

---
+++ juntando_turmas.js
@@ -0,0 +1,5 @@
+const salaA = ["Vitor", "Leandro", "José Wilker", "Andrew", "João Victor", "Ian"];
+const salaB = ["Samuel", "Illana", "Rita", "Natan"];
+const salasUnificadas = salaA.concat(salaB);
+
+console.log(salasUnificadas);
