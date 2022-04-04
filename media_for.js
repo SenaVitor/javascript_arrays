afc361c35a63787c4af5ed59ad51fdd2178ab229
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-03 23:16:33 -0300

    Using the for loop to calculate the average of a two-dimensional array

--- media_for.js
+++ media_for.js
@@ -1,7 +1,17 @@
-const notas = [10, 6.5, 7.5, 8];
-let soma = 0;
-for(let i = 0; i < notas.length; i++){
-    soma += notas[i];
+const notas1 = [10, 6.5, 8 ,7.5]
+const notas2 = [9, 6, 6]
+const notas3 = [8.5, 9.5]
+
+const notasGerais = [notas1,notas2,notas3]
+
+let media = 0
+
+for (let i = 0; i < notasGerais.length; i++) {
+  for (let j = 0; j < notasGerais[i].length; j++) {
+    media += notasGerais[i][j]/notasGerais[i].length;
+  }
 }
-let media = soma/notas.length;
-console.log(`A média é ${media}`);
+
+media = media/notasGerais.length
+
+console.log(media)
